2d879cb8736a23707a40e12182c6f096a625a4e9
Moa <benjamsc@student.42.fr>
2024-11-06 22:19:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,64 +1,71 @@
 #!/bin/bash
 
+# Liste des paquets à installer sur Debian
 package_Debian="
-	stow
-	gcc
-	gdb
-	git
-	make
-	neofetch
-	bash
-	curl
-	nano
-	wget
-	valgrind
+  stow
+  gcc
+  gdb
+  git
+  make
+  neofetch
+  bash
+  curl
+  nano
+  wget
+  valgrind
 "
 
-modules = "
-	zsh
-	p10k
+# Modules de dotfiles à lier
+modules="
+  zsh
+  p10k
   nvim
-	profile
+  profile
 "
 
-DOTFILES_DIR="$HOME/dotfiles"
+DOTFILES_DIR="$HOME/.dotfiles"
 
+# Fonction pour vérifier et installer un paquet avec apt
 check_install() {
-	if ! pkg info "$1" >/dev/null 2>&1; then
-		echo "$1 not installed"
-		pkg install -y "$1"
-	else
-		echo "$1 already installed"
-	fi
+  if ! dpkg -s "$1" >/dev/null 2>&1; then
+    echo "$1 not installed"
+    sudo apt update && sudo apt install -y "$1"
+  else
+    echo "$1 already installed"
+  fi
 }
 
+# Fonction pour installer tous les paquets listés
 install_package() {
-	for package in $package_freeBSD; do
-		check_install "$package"
-	done
+  for package in $package_Debian; do
+    check_install "$package"
+  done
 }
 
+# Fonction pour créer des liens symboliques avec Stow
 create_link() {
-	cd "$DOTFILES_DIR"
-	for mod in $modules; do
-		stow "$mod"
-	done
+  cd "$DOTFILES_DIR"
+  for mod in $modules; do
+    stow "$mod"
+  done
 }
 
+# Fonction pour télécharger et configurer Neovim
 dl_nvim() {
+  mkdir -p ~/.dotfiles/other/
   cd ~/.dotfiles/other/
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
   chmod u+x nvim.appimage
   cd ~
 }
 
-if [ "$1" = "sart" ]; then
-	# Starting mode
-	install_package
+# Lancement du script en fonction du paramètre
+if [ "$1" = "start" ]; then
+  # Mode de démarrage
+  install_package
   create_link
   dl_nvim
-
 else
-	install_package
-	create_link
-
+  install_package
+  create_link
+fi
