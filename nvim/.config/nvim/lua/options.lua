a5323a3633ddbb1f745a67dab4ed9ffd852699fd
moa <benjamsc@student.42.fr>
2025-02-10 16:20:37 +0100

    new:

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,14 @@
+require "nvchad.options"
+
+-- add yours here!
+
+vim.opt.expandtab = false
+vim.opt.cindent = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.relativenumber = true
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
