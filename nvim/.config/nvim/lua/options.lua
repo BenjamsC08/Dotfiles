4acbbb4802005341551dfc0d07cf9378d75c985d
Benjamin Schulze <benjamsc@paul-f4Br3s5.clusters.42paris.fr>
2025-03-17 09:42:00 +0100

    better config NVIM

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -2,6 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 
+-- Dans un fichier options.lua ou dans votre fichier config.lua
 vim.opt.expandtab = false
 vim.opt.cindent = true
 vim.opt.autoindent = true
@@ -9,7 +10,14 @@ vim.opt.smartindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.relativenumber = true
-vim.opt.undofile = true
+
+
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.o.foldcolumn = '1'
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,58 +1,73 @@
 return {
-	{
-	"stevearc/conform.nvim",
-	-- event = 'BufWritePre', -- uncomment for format on save
-	opts = require "configs.conform",
-	},
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
 
-	-- These are some examples, uncomment them if you want to see them work!
-	-- {
-	"neovim/nvim-lspconfig",
-	config = function()
-	  require "configs.lspconfig"
-	end,
-	},
-
-{
-    "MoulatiMehdi/42norm.nvim",
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
     config = function()
-        -- On étend le PATH avec le dossier de ton venv (si nécessaire)
-
-        local norm = require("42norm")
-
-        -- Configuration du plugin 42norm
-        norm.setup({
-            header_on_save = true,
-            format_on_save = true,
-            liner_on_change = true,
-            -- Si le plugin propose une option pour définir le chemin de la commande,
-            -- tu peux la laisser ici. Sinon, Neovim devrait trouver 'norminette'
-            -- via le PATH que nous venons de mettre à jour.
-            -- norminette_cmd = "norminette",  -- Si nécessaire, tu peux spécifier le chemin ici
-        })
-
-        -- Les keymaps
-        vim.keymap.set("n", "<F5>", function()
-            norm.check_norms()
-        end, { desc = "Update 42norms diagnostics", noremap = true, silent = true })
-
-        vim.keymap.set("n", "<C-f>", function()
-            norm.format()
-        end, { desc = "Format buffer on 42norms", noremap = true, silent = true })
-
-        vim.keymap.set("n", "<F1>", function()
-            norm.stdheader()
-        end, { desc = "Insert 42header", noremap = true, silent = true })
+      require "configs.lspconfig"
     end,
-}
+  },
+	{
+		"MoulatiMehdi/42norm.nvim",
+		config = function()
+			local norm = require("42norm")
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
--- }
+			norm.setup({
+				header_on_save = true,
+				format_on_save = true,
+				liner_on_change = true,
+			})
+
+			-- Press "F5" key to run the norminette
+			vim.keymap.set("n", "<F5>", function()
+				norm.check_norms()
+			end, { desc = "Update 42norms diagnostics", noremap = true, silent = true })
+
+			vim.keymap.set("n", "<C-f>", function()
+				norm.format()
+			end, { desc = "Format buffer on 42norms", noremap = true, silent = true })
+
+			vim.keymap.set("n", "<F1>", function()
+				norm.stdheader()
+			end, { desc = "Insert 42header", noremap = true, silent = true })
+
+			-- create your commands
+			vim.api.nvim_create_user_command("Norminette", function()
+				norm.check_norms()
+			end, {})
+			vim.api.nvim_create_user_command("Format", function()
+				norm.format()
+			end, {})
+			vim.api.nvim_create_user_command("Stdheader", function()
+				norm.stdheader()
+			end, {})
+    end,
+	},
+	{
+	  "brenton-leighton/multiple-cursors.nvim",
+	  version = "*",  -- Use the latest tagged version
+	  opts = {},  -- This causes the plugin setup function to be called
+	  keys = {
+		{"<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = {"n", "i", "x"}, desc = "Add cursor and move up"},
+		{"<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = {"n", "i", "x"}, desc = "Add cursor and move down"},
+
+		{"<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = {"n", "i"}, desc = "Add or remove cursor"},
+
+		{"<Leader>m", "<Cmd>MultipleCursorsAddVisualArea<CR>", mode = {"x"}, desc = "Add cursors to the lines of the visual area"},
+
+		{"<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = {"n", "x"}, desc = "Add cursors to cword"},
+		{"<Leader>A", "<Cmd>MultipleCursorsAddMatchesV<CR>", mode = {"n", "x"}, desc = "Add cursors to cword in previous area"},
+
+		{"<C-q>", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = {"n", "x"}, desc = "Add cursor and jump to next cword"},
+		{"<Leader>D", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = {"n", "x"}, desc = "Jump to next cword"},
+
+		{"<Leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = {"n", "x"}, desc = "Lock virtual cursors"},
+	  },
+},
+
+}
