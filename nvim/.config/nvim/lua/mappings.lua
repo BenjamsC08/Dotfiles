91a083a01aed5e466791bc1d3b89dc8900a78177
Moa <benjamsc@student.42.fr>
2025-03-18 15:04:27 +0100

    Update mappings.lua

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -5,7 +5,6 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
 map("n", "<leader><Down>", "yyp", { desc = "Duplicate line below" })
 map("n", "<A-Down>", ":m +1<CR>", { desc = "Move line down" })
 map("n", "<A-Up>", ":m -2<CR>", { desc = "Move line up" })
